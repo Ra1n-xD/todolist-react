4f5262a283323540053194ada4a89ec70cbbd335
Ra1n <theraingame@mail.ru>
2023-05-28 07:06:55 +0300

    Исправдленны баги сортировки

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import { useState } from 'react';
 import AddTask from './components/AddTask/AddTask';
 import AppInfo from './components/AppInfo/AppInfo';
 import TasksList from './components/TasksList/TasksList';
 import TasksFilter from './components/TasksFilter/TasksFilter';
-
-import React, { useState } from 'react';
 
 import './App.css';
 
@@ -16,32 +15,28 @@ interface ITask {
 
 const App = () => {
   const [todos, setTodos] = useState<ITask[]>([
-    { id: Math.floor(Math.random() * 1000), title: 'Максим', completed: false, favorites: false },
-    { id: Math.floor(Math.random() * 1000), title: 'не', completed: false, favorites: false },
-    { id: Math.floor(Math.random() * 1000), title: 'лох', completed: false, favorites: false }
+    { id: Math.random() * 1000, title: 'Максим', completed: false, favorites: false },
+    { id: Math.random() * 1000, title: 'Не', completed: false, favorites: false },
+    { id: Math.random() * 1000, title: 'Лох', completed: false, favorites: false }
   ]);
-
-  const [visibleTodos, setVisibleTodos] = useState<ITask[]>(todos);
 
   const addTask = (titleTask: string) => {
     const newTask: ITask = {
-      id: Math.floor(Math.random() * 1000),
+      id: Math.random() * 1000,
       title: titleTask,
       completed: false,
       favorites: false
     };
 
     setTodos((prevTodos) => [newTask, ...prevTodos]);
-    setVisibleTodos((prevTodos) => [newTask, ...prevTodos]);
   };
 
   const deleteTask = (id: number) => {
     setTodos((prevTodos) => prevTodos.filter((task) => task.id !== id));
-    setVisibleTodos((prevTodos) => prevTodos.filter((task) => task.id !== id));
   };
 
   const completeTask = (id: number) => {
-    const complete = (prevTodos: any) => {
+    setTodos((prevTodos) => {
       const newTodos = prevTodos.map((task: any) => {
         if (task.id === id) {
           return { ...task, completed: !task.completed, favorites: false };
@@ -52,42 +47,45 @@ const App = () => {
       const completedTasks = newTodos.filter((task: any) => task.completed);
       const activeTasks = newTodos.filter((task: any) => !task.completed);
       return [...activeTasks, ...completedTasks];
-    };
-
-    setTodos((prevTodos) => complete(prevTodos));
-    setVisibleTodos((prevTodos) => complete(prevTodos));
+    });
   };
 
   const favoritesTask = (id: number) => {
-    const favorites = (prevTodos: any) => {
-      return prevTodos.map((task: any) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((task) => {
         if (task.id === id) {
           return { ...task, favorites: !task.favorites };
         }
         return task;
-      });
-    };
-
-    setTodos((prevTodos) => favorites(prevTodos));
-    setVisibleTodos((prevTodos) => favorites(prevTodos));
+      })
+    );
   };
 
-  const showAll = () => {
-    setVisibleTodos(todos);
+  const filterTasks = (filter: string) => {
+    switch (filter) {
+      case 'all':
+        return todos;
+      case 'incomplete':
+        return todos.filter((task) => !task.completed);
+      case 'favorites':
+        return todos.filter((task) => task.favorites);
+      default:
+        return todos;
+    }
   };
 
-  const showIncomplete = () => {
-    setVisibleTodos(todos.filter((task) => !task.completed));
+  const [filter, setFilter] = useState('all');
+
+  const handleFilterChange = (newFilter: string) => {
+    setFilter(newFilter);
   };
 
-  const showFavorites = () => {
-    setVisibleTodos(todos.filter((task) => task.favorites));
-  };
+  const visibleTodos = filterTasks(filter);
 
   return (
     <div className="app">
       <AppInfo todos={todos} />
-      <TasksFilter showAll={showAll} showIncomplete={showIncomplete} showFavorites={showFavorites} />
+      <TasksFilter handleFilterChange={handleFilterChange} selectedFilter={filter} />
       <AddTask addTask={addTask} />
       <TasksList todos={visibleTodos} deleteTask={deleteTask} completeTask={completeTask} favoritesTask={favoritesTask} />
     </div>
--- src/components/TasksFilter/TasksFilter.tsx
+++ src/components/TasksFilter/TasksFilter.tsx
@@ -2,12 +2,12 @@ import Button from '../Button/Button';
 
 import './TasksFilter.css';
 
-const TasksFilter = ({ showAll, showIncomplete, showFavorites }: any) => {
+const TasksFilter = ({ handleFilterChange, selectedFilter }: any) => {
   return (
     <div className="btn-group">
-      <Button name={'Все'} styleName={'btn btn-outline-light'} onClick={showAll} />
-      <Button name={'Не выполнено'} styleName={'btn btn-outline-light'} onClick={showIncomplete} />
-      <Button name={'Избранное'} styleName={'btn btn-outline-light'} onClick={showFavorites} />
+      <Button name={'Все'} styleName={`btn ${selectedFilter === 'all' ? 'btn-light' : 'btn-outline-light'}`} onClick={() => handleFilterChange('all')} />
+      <Button name={'Не выполнено'} styleName={`btn ${selectedFilter === 'incomplete' ? 'btn-light' : 'btn-outline-light'}`} onClick={() => handleFilterChange('incomplete')} />
+      <Button name={'Избранное'} styleName={`btn ${selectedFilter === 'favorites' ? 'btn-light' : 'btn-outline-light'}`} onClick={() => handleFilterChange('favorites')} />
     </div>
   );
 };
